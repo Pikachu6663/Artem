8a25fef25cdc901382c7c4fb5e50a602ca794c36
Артём Богначёв <elka6454@mail.ru>
2024-07-02 23:30:03 +0300

    Создание dll-библиотеки;

--- DllLibrary/Class1.cs
+++ DllLibrary/Class1.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace DllLibrary
 {
-    public class Class1
+    public class Garden
     {
+        public string Address { get; set; }
+        public string Owner { get; set; }
+        private List<Flower> Flowers = new List<Flower>();
+
+        public Garden(string address, string owner)
+        {
+            Address = address;
+            Owner = owner;
+
+        }
+        public void AddFlower(Flower flower)
+        {
+            Flowers.Add(flower);
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine($"Информация о саде:\n" +
+                $"Адрес: {Address}\n\n" +
+                $"Владелец: {Owner}\n" + "Цветы:");
+            Flowers.Sort();
+            foreach (Flower p in Flowers)
+            {
+                p.Show();
+            }
+        }
+    }
+    namespace GardenLibrary3
+    {
+        public class Flower : IComparable<Flower>
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+
+            public Flower(string title, string description)
+            {
+                Title = title;
+                Description = description;
+            }
+            public void Show()
+            {
+                Console.WriteLine($"Название: {Title}\n" +
+                    $"- Описание: {Description}\n");
+            }
+            public int CompareTo(Flower other)
+            {
+                return Title.CompareTo(other.Title);
+            }
+        }
     }
 }
